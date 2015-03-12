1dbcc24d5f87f0471e4baae3b5643cab4b775a8f
Reshef Elisha <reshef.elisha@gmail.com>
2015-03-12 19:20:15 -0400

    Make it work with Heroku

--- app.rb
+++ app.rb
@@ -1,30 +1,34 @@
 require 'sinatra'
 require 'slim'
 
-def swipes
-swipes = [Swipe.new, Swipe.new]
-swipes
-end
+module MarketSwipe
+  class App < Sinatra::Base
+	def swipes
+	swipes = [Swipe.new, Swipe.new]
+	swipes
+	end
 
-get '/' do
-  slim :index
-end
+	get '/' do
+	  slim :index
+	end
 
-class Swipe
-  def initialize(owner = User.new("rhe8","Reshef Elisha"), time = "000000")
-    @owner = owner
-	@time = time
+	class Swipe
+	  def initialize(owner = User.new("rhe8","Reshef Elisha"), time = "000000")
+		@owner = owner
+		@time = time
+	  end
+	  
+	  attr_reader :owner
+	  attr_reader :time
+	end
+
+	class User
+	  def initialize(pittUname, fullName)
+		@pittUname = pittUname
+		@fullName = fullName
+	  end
+	  attr_reader :pittUname
+	  attr_reader :fullName
+	end
   end
-  
-  attr_reader :owner
-  attr_reader :time
-end
-
-class User
-  def initialize(pittUname, fullName)
-    @pittUname = pittUname
-	@fullName = fullName
-  end
-  attr_reader :pittUname
-  attr_reader :fullName
 end
